c7e1c55ba6384bf57e4d9f75b24fb03b8ac711d8
Pragmatic <metaceo01@gmail.com>
2018-07-29 04:34:25 +0200

    earn money

--- views.go
+++ views.go
@@ -10,6 +10,12 @@ func homeView(ctx *macaron.Context) {
 	ctx.HTML(200, "home")
 }
 
+func profitView(ctx *macaron.Context) {
+	ctx.Data["Title"] = "Making Profit in Anonutopia | "
+
+	ctx.HTML(200, "profit")
+}
+
 func contactView(ctx *macaron.Context) {
 	ctx.Data["Title"] = "Contact | "
 
