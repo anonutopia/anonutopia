99b4689a3f69f51a4fcaddaded88e0a36bb06a8a
Pragmatic <cryptopragmatic@protonmail.com>
2018-09-25 00:36:29 +0200

    new menu layout

--- public/js/main.js
+++ public/js/main.js
@@ -89,6 +89,17 @@ var ttlSupply = -1;
 		animateOut: 'fadeOut'
 	});
 
+	$('#about-slider1').owlCarousel({
+		items:1,
+		loop:true,
+		margin:15,
+		nav: true,
+		navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+		dots : true,
+		autoplay : true,
+		animateOut: 'fadeOut'
+	});
+
 	$('#testimonial-slider').owlCarousel({
 		loop:true,
 		margin:15,
